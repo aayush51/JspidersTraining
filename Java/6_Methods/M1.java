dd0fe619cdd10c2d358b6d688287a8ff4134097e
Aayush More <asmaayush2018@gmail.com>
2024-03-16 14:27:37 +0530

    Methods Concept

---
+++ Java/6_Methods/M1.java
@@ -0,0 +1,19 @@
+class M1
+{
+	public static void main(String[] args) 
+	{
+		raju();
+		raju();
+		raju();
+		rani();
+	}
+	public static void raju() 
+	{
+		System.out.println("Hi! I am Raju");
+	}
+	public static void rani() 
+	{
+		System.out.println("Get Lost!");
+	}
+	
+}
