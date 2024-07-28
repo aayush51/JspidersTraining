73f2093610d1bcf9882c6195036c5decf2a790e3
Aayush More <asmaayush2018@gmail.com>
2024-07-29 01:42:08 +0530

    update

---
+++ Java/InterviewQuestions/VisionWaves/Que1.java
@@ -0,0 +1,91 @@
+package VisionWaves;
+
+public class Que1 {
+	
+/*
+ * Q1] What is class & object
+ * 
+ * Q2] What is constructor and what is real use of constructor 
+ * 
+ * Q3] What constructor overloading and constructor chaining
+ * 
+ * Q4] In how many ways we can create object
+ * 
+ * Q5] What are members of class
+ * 
+ * Q6] What are differences between static and non-static
+ * 
+ * Q7] Can we overload main method
+ * 
+ * Q8] Can we declare constructor as private , If yes then how can we perform instatiation for private constructor
+ * 
+ * Q9] Can we declare class as private 
+ * 
+ * Q10] Can we make class as static
+ * 
+ * Q11] Can we call methods in multi-line block (Static Block)
+ * 
+ * Q12] Can we have multiple class in one java file , if yes what should be filename
+ *	
+ * Q13] What is inheritance and main purpose of inheritance
+ * 
+ * Q14] Can we override main method in java 
+ * 
+ * Q15] Can we override private methods in java
+ * 
+ * Q16] Can we access static variable by object creation , if yes what is use 
+ * 
+ * Q17] What is method overriding ? explain in terms of compile time and runtime
+ * 
+ * Q18] Can we declare constructors in abstract class
+ * 
+ * Q19] Can we have static methods in abstract class
+ * 
+ * Q20] Can we have main method in abstract class
+ * 
+ * Q21] Can we create object of interface / perform instantiation interface
+ * 
+ * Q22] In interface what are members that can be declared
+ * 
+ * Q23] What is multiple inheritance in interface
+ * 
+ * Q24] What are differences between exception and error
+ * 
+ * Q25] What is null pointer exception
+ * 
+ * Q26] What is interrupted exception
+ * 
+ * Q27] What is difference between throw and throws keyword
+ * 
+ * Q28] What is difference between final keyword and finally block
+ * 
+ * Q29] What is difference between final keyword and finalize() method
+ * 
+ * Q30] What is purpose of equals() method
+ * 
+ * Q31] What are types of interfaces
+ * 
+ * Q32] Why we use functional interface
+ * 
+ * Q33] In how many ways we can create threads (multithreading,thread class,runable interface)
+ * 
+ * Q34] What is synchronization , in how many ways we can perform synchronization 
+ * 
+ * Q35] Explain built in packages in java
+ * 
+ * Q36] Explain runtime polymorphism with upcasting 
+ * 
+ * Q37] Explain compile time polymorphism with upcasting
+ * 
+ * Q38] Difference between equals() method and == operator
+ * 
+ * Q39] Difference between StringBuffer and StringBuilder
+ * 
+ * Q40] Explain some built in methods of String class
+ * 
+ * Q41] Explain some built in methods of StringBuilder class
+ * 
+ * Q42] Explain some built in methods of StringBuffer class
+ * 
+ */ 
+}
