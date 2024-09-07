efa68f5379eb2902b88f02afeb21b517851d19d1
Aayush More <asmaayush2018@gmail.com>
2024-09-07 23:41:06 +0530

    Output
    how get output
    different ways of getting output

---
+++ JavaScript/03_Output/Day9/demo.js
@@ -0,0 +1,20 @@
+console.log("Hello");
+console.error("Hello");
+console.warn("Hello");
+
+window.console.log("again hello");
+window.console.error("again error");
+window.console.warn("again warning");
+
+// output on html document / page 
+window.document.write("Here is write on document from window");
+
+// alert gives popup in browser window like prompt
+// alert("Hey whats happening !!!");
+
+// gives popup like alert
+// confirm("This is confirmation !")
+
+
+console.log(window);
+
