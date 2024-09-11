2a05ad67fbe49f99cac3808ae90424df1fd3baae
Aayush More <asmaayush2018@gmail.com>
2024-09-11 23:14:51 +0530

    update

---
+++ Servlet/Servlet/src/main/java/com/qsp/HttpExample.java
@@ -0,0 +1,11 @@
+package com.qsp;
+
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HttpExample extends HttpServlet {
+	public static void main(String[] args) {
+		
+	}
+
+}
