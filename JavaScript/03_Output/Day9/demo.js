ea73db79798c5c021e8b6bbc6bd0d3bce3b1f048
Aayush More <asmaayush2018@gmail.com>
2024-09-20 03:01:14 +0530

    window object

--- JavaScript/03_Output/Day9/demo.js
+++ JavaScript/03_Output/Day9/demo.js
@@ -8,6 +8,9 @@ window.console.warn("again warning");
 
 // output on html document / page 
 window.document.write("Here is write on document from window");
+// window.document.write("hello from JS file");
+// window.alert("Kaise ho aap log..........");
+// window.confirm("Click OK");
 
 // alert gives popup in browser window like prompt
 // alert("Hey whats happening !!!");
@@ -18,3 +21,11 @@ window.document.write("Here is write on document from window");
 
 console.log(window);
 
+// console.log(document);
+
+
+
+// Experiment ----------->
+console.log(a);
+var a = 10;
+console.log(confirm);
