b41c8378f5ec584c72c4366373065375a13daf43
Aayush More <asmaayush2018@gmail.com>
2024-10-18 13:07:45 +0530

    DOM

---
+++ JavaScript/14_DOM/Day36/demo.js
@@ -0,0 +1,12 @@
+console.log(document);
+
+// console.log(typeof document);   //object
+
+let h1 = document.getElementById("h1tag");
+console.log(h1);
+
+// console.log(typeof h1); //object
+
+h1.title="heading from dom";
+h1.style.color="red";
+h1.style.backgroundColor = "yellow";
